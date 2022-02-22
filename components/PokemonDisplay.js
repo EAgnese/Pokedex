258e73ed8b421eac9f69b1562af68221bb8885de
Agnese <neutron34980@gmail.com>
2022-02-22 21:07:16 +0100

    créa component Pokemon + test recup nb Pokemon

---
+++ components/PokemonDisplay.js
@@ -0,0 +1,15 @@
+app.component('pokemon-display',{
+    template:
+    /*html*/
+    `<div class="pokemon-display">
+      <div class="pokemon-container">
+        <div class="pokemon-image">
+        </div>
+        <div class="pokemon-info">
+          <h1>Issou</h1>
+  
+          <p v-else>Out of Stock</p>
+        </div>
+      </div>
+    </div>`
+})
--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const P = new Pokedex.Pokedex()
 
+const pokemons = P.getPokemonsList().then( res => {
+    res.count
+})
+
+
 const app = Vue.createApp({
+    data(){
+        return {pokemons : pokemons}
+    },
+    
+    methods :{
+        async infos(n){
+            const P = new Pokedex.Pokedex()
+            for(var i=1;i<=n;i++){
+                const pokemon = await P.getPokemonByName(i)
+                this.pokemons.push(pokemon)
+            }
+            console.log(this.pokemons[0].sprites.front_default)
+        },
+    },
 
 })
+
+console.log(app.pokemons)
